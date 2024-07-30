fc290bb67745d56692513451b3f7025a55a03f9f
kuniagps <skittogpbl@gmail.com>
2024-07-30 19:58:04 +0600

    Add files via upload

---
+++ script/nicescroll.js
@@ -0,0 +1,27 @@
+
+					$(document).ready(function() {
+					$("#download1").niceScroll({
+					cursorwidth: "6px",
+					cursorcolor: "#FFF",
+					autohidemode: "false"
+					});
+					});
+
+
+                  $(document).ready(function() {
+                    $("#link2").niceScroll({
+                      cursorwidth: "6px",
+                      cursorcolor: "#FFF",
+                      autohidemode: "false"
+                    });
+	});
+
+
+
+                  $(document).ready(function() {
+                    $("#link1").niceScroll({
+                      cursorwidth: "6px",
+                      cursorcolor: "#FFF",
+                      autohidemode: "false"
+                    });
+	});
